a0a57e38ec6b31bc2ac4b4ae8ecbd33ba6dd21c0
Trinity <trinityanne.martinez@gmail.com>
2022-03-02 19:07:28 -0800

    added some data vis ideas

--- MiscInfo.py
+++ MiscInfo.py
@@ -1,4 +1,25 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 
-# 1) Did a gender pay gap exist during our specified time period?
+# Have challenge goals tie back to research questions (how are we gonna implement
+# machine learning model and data visualizations)
+
+# machine learning: look at major features that affect our model, 
+# how to get best performance (depth of trees)
+
+# testing our code: use assert_equals 
+    # break down our data into helper functions
+    # create smaller dataset and test expected output against this
+
+
+# https://scikit-learn.org/stable/index.html
+# https://learn.g2.com/cross-validation
+# https://courses.cs.washington.edu/courses/cse163/software/
+
+
+# DATA VIS WE CAN CREATE:
+#  Which occupations result in the largest pay gap between men and women?
+
+# How do attributes other than gender (such as ethnicity, socio-economic 
+# status, immigration status) contribute to the pay gap? Which attributes 
+# result in the largest disparity?
+
+# What was the change over time for pay for men vs. women?
