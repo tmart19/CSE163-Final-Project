43390745228ea3258b06ccfd2592e94561669f40
Trinity <trinityanne.martinez@gmail.com>
2022-03-14 11:17:20 -0700

    updated the read me file

--- viz_code/DataVis.py
+++ viz_code/DataVis.py
@@ -26,7 +26,7 @@ def gender_annual_income_pie(df):
     """
     gender_annual_income = alt.Chart(df).mark_arc().encode(
         theta=alt.Theta(field="annlabinc", type="quantitative"),
-        color=alt.Color(field="sex", type="nominal", 
+        color=alt.Color(field="sex", type="nominal",
                         legend=alt.Legend(title="Sex (Male=1, Female=2)")),
     ).properties(
         width=300, height=300,
@@ -40,7 +40,7 @@ def gender_bonus_pay_bar(gp):
     Produces a bar chart comparing bonus pay and gender.
     """
     gender_bonus_amount = alt.Chart(gp).mark_bar(
-    color='steelblue'
+        color='steelblue'
     ).encode(
         x=alt.X('Gender', axis=alt.Axis(title='Gender (Male or Female)')),
         y=alt.Y('Bonus', axis=alt.Axis(title='Bonus Amount (in USD)'))
@@ -52,16 +52,24 @@ def gender_bonus_pay_bar(gp):
     # gender_bonus_amount.save('BonusPayGender.png')
 
 
-# Research Question #2: How does the gender pay gap differ across degree levels achieved for each gender? 
+# Research Question #2: How does the gender pay gap differ across degree
+# levels achieved for each gender?
 def ann_income_degree_bar(df):
     """
-    Produces a bar chart comparing annual income and degree obtained, grouped by gender.
+    Produces a bar chart comparing annual income and degree obtained,
+     grouped by gender.
     """
-    gender_ann_income_degree = alt.Chart(df, title="Annual Income by Sex and Degree Obtained").mark_bar().encode(
+    gender_ann_income_degree = alt.Chart(
+        df,title="Annual Income by Sex and Degree Obtained"
+        ).mark_bar().encode(
         x=alt.X('sex:N', axis=alt.Axis(title='Sex (1=Male, 2=Female)')),
-        y=alt.Y('annlabinc:Q', axis=alt.Axis(title='Annual Income in USD')),
-        color=alt.Color('sex:N', legend=alt.Legend(title="Sex by Color (1=Male, 2=Female)"), scale=alt.Scale(scheme='darkmulti')),
-        column=alt.Column('degree:O', title="Degree Levels (0=No College Degree, 1=Bachelor's, 2=Advanced)"),
+        y=alt.Y('annlabinc:Q',
+                axis=alt.Axis(
+                    title='Annual Income in USD')),
+        color=alt.Color('sex:N', legend=alt.Legend(title="Sex by Color (1=Male, 2=Female)"), 
+                        scale=alt.Scale(scheme='darkmulti')),
+        column=alt.Column('degree:O', 
+                        title="Degree Levels (0=No College Degree, 1=Bachelor's, 2=Advanced)"),
     ).properties(
         width=200,
         height=700
@@ -72,17 +80,19 @@ def ann_income_degree_bar(df):
 
 def base_pay_education_level(gp):
     """
-    Produces a bar chart comparing base pay and education level attained, grouped by gender.
+    Produces a bar chart comparing base pay and education level attained, 
+    grouped by gender.
     """
-    gender_base_pay_education = alt.Chart(gp, title="Base Pay by Degree and Sex").mark_bar().encode(
-        x=alt.X('Education', axis=alt.Axis(title='Degree')),
-        y=alt.Y('BasePay:Q', axis=alt.Axis(title='Annual Income in USD')),
-        color=alt.Color('Gender', legend=alt.Legend(title="Sex by Color")),
-        column=alt.Column('Gender', title="Gender"),
-    ).properties(
+    gender_base_pay_education = alt.Chart(gp, 
+        title="Base Pay by Degree and Sex").mark_bar().encode(
+            x=alt.X('Education', axis=alt.Axis(title='Degree')),
+            y=alt.Y('BasePay:Q', axis=alt.Axis(title='Annual Income in USD')),
+            color=alt.Color('Gender', legend=alt.Legend(title="Sex by Color")),
+            column=alt.Column('Gender', title="Gender"),
+        ).properties(
         width=200,
         height=700
-    )
+        )
     gender_base_pay_education.configure_title(fontSize=28)
     # gender_base_pay_education.save('BasePayEducationLevel.png')
 
